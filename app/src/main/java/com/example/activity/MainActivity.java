a1d7b068d8e0cf0bbe0c79869968231f281c53a2
Vshagina <shagina05@inbox.ru>
2023-02-25 16:20:34 +0300

    init commit

---
+++ app/src/main/java/com/example/activity/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "StartActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "onCreate()", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onCreate");
+    }
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Toast.makeText(this, "onRestoreInstanceState()", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onRestoreInstanceState");
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart()", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onStart");
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        Toast.makeText(this, "onResume()", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onResume");
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Toast.makeText(this, "onSaveInstanceState()", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onSaveInstanceState");
+    }
+        @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause()", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop()", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onStop");
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onRestart()", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onRestart");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy()", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onDestroy");
+    }
+}
